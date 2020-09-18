c2851664e826a723f084314e5eef8a1f800c91cb
Leonardo Oliveira <lleonardo@equinix.com>
2020-09-17 23:05:58 -0300

    Initial Commit

---
+++ src/database/migrations/1600367177885-ForeignKeys.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class ForeignKeys1600367177885 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategory',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+  }
+}
